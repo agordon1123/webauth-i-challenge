ddf8bd2b9c8f8e7ee865679157a039eedcb89d48
Alex <agordon1123@gmail.com>
2019-09-16 18:17:41 -0400

    created endpoints

--- api/models/user-model.js
+++ api/models/user-model.js
@@ -2,18 +2,27 @@ const db = require('../../data/db-config');
 
 module.exports = {
     addUser,
-    getUsers
+    getUsers,
+    login
 };
 
-const addUser = (user) => {
+function addUser(user) {
     return db('users')
-        .insert(user)
-        .then(success => success)
-        .catch(err => err);
+    .insert(user)
+    .then(success => success)
+    .catch(err => err);
 };
 
-const getUsers = () => {
+function getUsers() {
     return db('users')
-        .then(users => users)
+    .then(users => users)
+    .catch(err => err);
+};
+
+function login(credentials) {
+    const { username, password } = credentials;
+    return db('users')
+        .where({ username })
+        .then(user => user)
         .catch(err => err);
 };
--- api/routers/user-router.js
+++ api/routers/user-router.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+
 const Users = require('../models/user-model');
+
 const validateUserBody = require('../middleware/validate-user-body');
+const validatePassword = require('../middleware/validate-credentials');
 
 const router = express.Router();
 
 router.post('/register', validateUserBody, (req, res) => {
-    Users.addUser(req.body)
+    let { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+
+    Users.addUser({ username, password: hash })
         .then(success => res.status(201).json(success))
         .catch(err => res.status(500).json(err));
 });
 
-router.post('/login', (req, res) => {
-
+router.post('/login', validatePassword, (req, res) => {    
+    Users.login(req.body)
+        .then(user => res.status(201).json(user))
+        .catch(() => res.status(500).json({ error: 'Internal error' }));
 });
 
 router.get('/users', (req, res) => {
